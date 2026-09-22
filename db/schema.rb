04c549e474f7e4f20fecadb8242f36d0e7e1f442
DH <151378521+Kezy-d4@users.noreply.github.com>
2026-09-22 19:06:11 -0400

    feat: add post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_09_22_023921) do
+ActiveRecord::Schema[8.1].define(version: 2026_09_23_024911) do
+  create_table "posts", force: :cascade do |t|
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.string "title"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.string "email"
@@ -18,4 +27,6 @@ ActiveRecord::Schema[8.1].define(version: 2026_09_22_023921) do
     t.string "password"
     t.datetime "updated_at", null: false
   end
+
+  add_foreign_key "posts", "users"
 end
