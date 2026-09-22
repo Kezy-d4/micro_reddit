7bac89a31fcb68fb99812dfc55f18ee2eaa221eb
DH <151378521+Kezy-d4@users.noreply.github.com>
2026-09-22 18:13:48 -0400

    feat: add user model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  validates :name, :email, :password, presence: true
+  validates :name, :email, uniqueness: true
+  validates :name, length: { in: 2..30 }
+  validates :password, length: { minimum: 8 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
