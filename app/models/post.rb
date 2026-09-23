e4f8cd83ac8da563af07ed8658d42c863926c503
DH <151378521+Kezy-d4@users.noreply.github.com>
2026-09-23 12:26:53 -0400

    fix: ensure post validates user foreign key

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, :body, presence: true
+  validates :title, :body, :user_id, presence: true
   validates :title, length: { maximum: 200 }
   validates :body, length: { maximum: 1000 }
 
